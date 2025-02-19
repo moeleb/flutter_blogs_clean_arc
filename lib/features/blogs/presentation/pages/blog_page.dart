fbeb986fe0dd81990e804704431dc1aa7e92449f
mohamad kanafani <mkanafani40@gmail.com>
2025-02-19 10:54:23 +0200

    Created blog page

--- lib/features/blogs/presentation/pages/blog_page.dart
+++ lib/features/blogs/presentation/pages/blog_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/blogs/presentation/pages/add_new_blog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,9 @@ class BlogPage extends StatelessWidget {
         title: const Text('Blog App'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(context, AddNewBlogPage.route());
+            },
             icon: const Icon(CupertinoIcons.add_circled),
           ),
         ],
